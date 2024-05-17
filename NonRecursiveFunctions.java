dada51b763a2acbb2c019999c0ae2e82563b2aeb
jimatlang <jim.stewart@thelangschool.org>
2024-05-17 10:57:59 -0400

    added error to show call stack

--- NonRecursiveFunctions.java
+++ NonRecursiveFunctions.java
@@ -1,11 +1,14 @@
 public class NonRecursiveFunctions {
     public void FunctionA(){
         System.out.println("running function A");
+        functionB();
     }
-    private void FunctionB(){
-        System.out.println("running function A");
+    private void functionB(){
+        System.out.println("running function B");
+        functionC();
     }
     private void functionC(){
         System.out.println("Running function C");
+        int x = 1/0;
     }
 }
